1857d957be4e147a39b285c85a27e0b8b619d821
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-22 22:31:27 +0200

    Update

--- _other/guardians/guardians.py
+++ _other/guardians/guardians.py
@@ -1,3 +1,4 @@
 # A simple platform game is organized as a network of underground tunnels that intersect. 
 # At each intersection, there are guardian robots that need to be overcome in order to 
 # continue moving forward. Each tunnel is of the same length, and it takes the same 
+# amount of time to overcome each guardian. At the beginning of the game, two players 
