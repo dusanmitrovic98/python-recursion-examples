6cdaa60bcc34f129d823934b82a9c962e5061144
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-23 22:45:51 +0200

    Update

--- _other/guardians/guardians.py
+++ _other/guardians/guardians.py
@@ -2,3 +2,4 @@
 # At each intersection, there are guardian robots that need to be overcome in order to 
 # continue moving forward. Each tunnel is of the same length, and it takes the same 
 # amount of time to overcome each guardian. At the beginning of the game, two players 
+# are located at different intersections and want to join forces to overcome the guardians 
