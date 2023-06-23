2d635bd97e3927ad20d52b176bc8fa6a78998d69
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-23 23:14:55 +0200

    Update guardians.py

--- _other/guardians/guardians.py
+++ _other/guardians/guardians.py
@@ -3,3 +3,4 @@
 # continue moving forward. Each tunnel is of the same length, and it takes the same 
 # amount of time to overcome each guardian. At the beginning of the game, two players 
 # are located at different intersections and want to join forces to overcome the guardians 
+# more easily. Both players move at the same speed through the tunnels. Write a function named 
