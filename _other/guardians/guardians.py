c5c10192c4d330fc283d275156ceaae4e8f9e924
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-23 22:44:00 +0200

    Update

--- _other/guardians/guardians.py
+++ _other/guardians/guardians.py
@@ -1,15 +1,1 @@
 # A simple platform game is organized as a network of underground tunnels that intersect. 
-# At each intersection, there are guardian robots that need to be overcome in order to 
-# continue moving forward. Each tunnel is of the same length, and it takes the same 
-# amount of time to overcome each guardian. At the beginning of the game, two players 
-# are located at different intersections and want to join forces to overcome the guardians 
-# more easily. Both players move at the same speed through the tunnels. Write a function named 
-# 'join(map, intersection1, intersection2)' in the Python programming language that uses a 
-# search algorithm to determine, based on the initial intersections of both players 
-# (intersection1 and intersection2), the minimum number of guardians they need to overcome
-# in order to join at one of the intersections in the game defined by the graph map." 
-# Loops are forbidden! Only recursion! 
-
-def join(map, intersection1, intersection2, visited=None):
-    if visited is None:
-        visited = set()  # Initialize the set of visited intersections
