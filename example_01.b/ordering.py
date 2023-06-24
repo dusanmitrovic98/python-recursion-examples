5139a50f829f7c267a9918c8a71ecb2d99273ffe
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-24 22:33:38 +0200

    Update ordering.py

--- example_01.b/ordering.py
+++ example_01.b/ordering.py
@@ -1,2 +1,3 @@
 # Define function 'ordering', which combines 2 lists of numbers into one list consisting of tuple objects that have three elements. 
 # The first element of the resulting collection is an element of the first list, the second is an element of the 
+# second list, and the third is either 'Yes' if the second list element is twice as large as the first list element,
