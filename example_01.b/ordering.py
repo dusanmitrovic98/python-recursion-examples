188025994c520df0964de20adbeba67c763d5316
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-24 22:04:57 +0200

    Update

--- example_01.b/ordering.py
+++ example_01.b/ordering.py
@@ -2,3 +2,4 @@
 # The first element of the resulting collection is an element of the first list, the second is an element of the 
 # second list, and the third is either 'Yes' if the second list element is twice as large as the first list element,
 # or 'No' if it is not. The length of the list is the dimension of the longer of the two input lists. The nth tuple 
+# data of the resulting collection consists of the nth numbers from both input lists, where the smaller and larger 
