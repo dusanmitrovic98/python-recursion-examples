f48bd18097e2a4aa862940f356eaf3081727b41b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-25 21:46:25 +0200

    Update breadth_first_search.py

--- _other/breadth_first_search/breadth_first_search.py
+++ _other/breadth_first_search/breadth_first_search.py
@@ -1,16 +1,1 @@
 import queue;
-
-def breadth_first_search(graph, start, end):
-    if start is end:
-        path = list()
-        path.append(start)
-        return path
-    queue_nodes = queue.Queue(len(graph))
-    visited = set()
-    prev_nodes = dict()
-    prev_nodes[start] = None
-    visited.add(start)
-    queue_nodes.put(start)
-    found_dest = False
-    while (not found_dest) and (not queue_nodes.empty()):
-        node = queue_nodes.get()
